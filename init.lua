9172e488fc41f633bf337452b414682b93230a99
Jannik <jschl@DESKTOP-QU3AT8L>
2024-03-26 22:13:04 +0100

    Add html to standard lsp

--- init.lua
+++ init.lua
@@ -540,6 +540,7 @@ require('lazy').setup({
         gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
+        html = { filetypes = { 'html', 'twig', 'hbs' } },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
