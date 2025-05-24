e48bfb5f209992987c59368d8cacebb97437ef52
Jannik Schlueter <jannikschlueter91@gmail.com>
2025-05-24 18:36:52 +0200

    add hardtime

--- init.lua
+++ init.lua
@@ -1063,7 +1063,13 @@ require('lazy').setup({
       end, { desc = 'Harpoon: Next file' })
     end,
   },
-  {"ellisonleao/glow.nvim", config = true, cmd = "Glow"},
+  { 'ellisonleao/glow.nvim', config = true, cmd = 'Glow' },
+  {
+    'm4xshen/hardtime.nvim',
+    lazy = false,
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {},
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
